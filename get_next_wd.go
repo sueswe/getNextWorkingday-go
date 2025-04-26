279c49e8c986f72554a7a6f295e4ec70702b6053
Werner Süß <nookiesuess@gmail.com>
2025-04-26 10:52:49 +0200

    WIP: searching for a holiday

--- get_next_wd.go
+++ get_next_wd.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -13,7 +14,8 @@ func CheckErr(e error) {
 	}
 }
 
-func workingDay(theDate time.Time) (string, int) {
+// returns 0 when workday
+func isWorkingDay(theDate time.Time) (string, int) {
 	// check date for weekend or working day
 	i := 0
 	dow := theDate.Weekday()
@@ -29,18 +31,48 @@ func workingDay(theDate time.Time) (string, int) {
 	return dow.String(), i
 }
 
+// returns 0 when holiday, else 1
+func isHoliday(theDate string, csv string) int {
+	// check if date is a holoday:
+	f, err := os.ReadFile(csv)
+	CheckErr(err) // Splits on newlines by default.
+	rtc := 1
+	//log.Println("File " + csv + " opened.")
+	// fmt.Println(f)
+	// os.Stdout.Write(f)
+	read_lines := strings.Split(string(f), "\n")
+	for _, line := range read_lines {
+		// fmt.Println(line) //print the content line by line
+		// log.Println("Checking " + theDate + " against: " + line)
+		if strings.Contains(line, theDate) {
+			log.Println("Found: " + line)
+			rtc = 0
+		}
+	}
+	return rtc
+}
+
 func main() {
+	if len(os.Args) <= 1 {
+		log.Fatalln("Missing arguments.")
+		os.Exit(1)
+	}
+
 	dateStr := os.Args[1]
 	plus := os.Args[2]
 	p, _ := strconv.Atoi(plus)
-
+	dataFileName := "holiday.csv"
 	layout := "2006-01-02"
+
 	d, err := time.Parse(layout, dateStr)
 	CheckErr(err)
+	log.Println("Startdate: " + d.String())
+	targetDate := d.AddDate(0, 0, p)
+	log.Println("Calculated: " + targetDate.String())
 
-	n := d.AddDate(0, 0, p)
-	log.Println(n)
+	wochentag, _ := isWorkingDay(targetDate)
+	log.Println("Is a: " + wochentag)
 
-	wochentag, _ := workingDay(d)
-	log.Println(wochentag)
+	checkDateStr := targetDate.String()
+	isHoliday(checkDateStr, dataFileName)
 }
