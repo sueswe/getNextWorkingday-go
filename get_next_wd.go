fbc2aab90291bc2e4921b70652db15ad3ff8099b
Werner Süß <nookie.suess@gmail.com>
2025-05-01 13:31:34 +0200

    EXTEND: check for the configfile.

--- get_next_wd.go
+++ get_next_wd.go
@@ -18,7 +18,7 @@ func CheckErr(e error) {
 }
 
 var REV = "DEV"
-var VERSION = "0.2"
+var VERSION = "0.3"
 var home string = os.Getenv("HOME")
 
 func usage() {
@@ -83,6 +83,20 @@ func nextWorkingDay(csv string, datum string) {
 	//return datum
 }
 
+// check for a ConfigFile
+func readConfig(filename string) int {
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	_, err := os.ReadFile(filename)
+	if err != nil {
+		infoLog.Println("(no config " + filename + " found)")
+		return 1
+	} else {
+		infoLog.Println("Reading config: " + filename)
+		return 0
+	}
+
+}
+
 func main() {
 	if len(os.Args) <= 1 {
 		usage()
@@ -94,8 +108,12 @@ func main() {
 	plus := os.Args[2]
 	p, _ := strconv.Atoi(plus)
 
+	if readConfig(home+"/.getNextWorkingday.toml") == 1 {
+		log.Fatalln("ERROR: Cannot find the configfile. Please read the README.")
+	}
 	// Get from configfile:
 	config, err := toml.LoadFile(home + "/.getNextWorkingday.toml")
+	CheckErr(err)
 	//dataFileName := "holiday.csv"
 	dataFile := config.Get("default.HOLIDAY_FILE").(string)
 	dataPath := config.Get("default.HOLIDAY_PATH").(string)
